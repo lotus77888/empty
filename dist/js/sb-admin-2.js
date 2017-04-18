3b3384fbdbd3d1b086ed0a12e5ce5e93d28cfaf9
ashok <ashok.kurukuntla@lorventindia.com>
2017-04-19 00:09:10 +0530

    My site ready for deployment.

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -1,7 +1,7 @@
 /*!
- * Start Bootstrap - SB Admin 2 v3.3.7+1 (http://startbootstrap.com/template-overviews/sb-admin-2)
- * Copyright 2013-2017 Start Bootstrap
- * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
+ * Start Bootstrap - govin admin v3.3.7+1 ()
+ * Copyright 2013-2017 Govin
+ * Licensed under MIT ()
  */
 $(function() {
     $('#side-menu').metisMenu();
